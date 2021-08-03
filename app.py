be6fc5d57b68f01796f1d7a1061c77882dc4d1ad
Jorge Romero <jorge@motoinsight.com>
2021-08-03 10:24:18 +0000

    black, pylint and code tools

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import datetime
 import concurrent.futures
+import datetime
 import logging
 from collections import Counter
 from dataclasses import dataclass
@@ -8,11 +8,12 @@ import hug
 import requests
 from falcon import HTTPServiceUnavailable
 
-
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s::%(funcName)s:%(lineno)d - %(message)s')
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(filename)s::%(funcName)s:%(lineno)d - %(message)s"
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
@@ -25,7 +26,7 @@ class MusicBrainzException(Exception):
 @dataclass
 class ReleaseApiResponse:
     response_data: dict
-    
+
     @property
     def total_items(self):
         return self.response_data.get("release-count", 0)
@@ -54,19 +55,18 @@ class ReleaseGroupApiResponse:
             year = datetime.date.fromisoformat(isodate).year
         except (TypeError, ValueError):
             year = None
-        
+
         return year
 
 
 class MusicBrainzClient:
     BASE_API_URL = "https://musicbrainz.org/ws/2"
-    
+
     def __init__(self):
         self.client = requests.Session()
-        self.client.headers.update({
-            "Accept": "application/json",
-            "User-Agent": "musicbrainz-proxy/1.0.0"
-        })
+        self.client.headers.update(
+            {"Accept": "application/json", "User-Agent": "musicbrainz-proxy/1.0.0"}
+        )
 
     def browse_releases(self, artist_mbid, limit=25, offset=0):
         """Make an API call to the Browse endpoint looking for an artist's releases.
@@ -77,7 +77,7 @@ class MusicBrainzClient:
         > request supports an 'offset=' argument to get more results. Browse
         > requests also support 'limit=': the default limit is 25, and you can
         > increase that up to 100.
-        
+
         """
         url = (
             f"{self.BASE_API_URL}/release/"
@@ -93,9 +93,9 @@ class MusicBrainzClient:
             response = self.client.get(url, timeout=3)
             response.raise_for_status()
             response_data = response.json()
-        except (requests.RequestException, ValueError):
+        except (requests.RequestException, ValueError) as exc:
             logger.exception("HTTP GET to %s failed!", url)
-            raise MusicBrainzException
+            raise MusicBrainzException from exc
         else:
             logger.info("HTTP GET to %s", url)
 
@@ -103,7 +103,7 @@ class MusicBrainzClient:
 
     def get_releases(self, artist_mbid):
         """Fetch all the artist's releases.
-        
+
         We're leveraring the Browse endpoint which supports up to 100 results per
         page, so getting all the albums will likely require multiple requests.
 
@@ -114,7 +114,7 @@ class MusicBrainzClient:
         releases will take one single request.
         """
         releases = []
-        
+
         # Make this first request, the total number of releases can be found there
         limit = 100
         offset = 0
@@ -123,7 +123,7 @@ class MusicBrainzClient:
         except MusicBrainzException:
             logger.exception("Could not fetch all releases for %s", artist_mbid)
             raise
-            
+
         releases.extend(response.releases)
         total_releases = response.total_items
         offset += limit
@@ -146,10 +146,10 @@ class MusicBrainzClient:
 
 @hug.local()
 @hug.cli()
-@hug.get(examples='mbid=65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab&offset=0&limit25')
+@hug.get(examples="mbid=65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab&offset=0&limit25")
 def albums(mbid: hug.types.text, limit: int = 50, offset: int = 0):
     """HTTP API endpoint that returns an artist's release groups.
-    
+
     In MusicBrainz's terminology a 'release group' is what's more commonly known
     as 'album'. A 'release' is an instance of a 'release group' you can buy as
     CD, vinyl, etc.
@@ -159,38 +159,37 @@ def albums(mbid: hug.types.text, limit: int = 50, offset: int = 0):
         releases = MusicBrainzClient().get_releases(mbid)
     except MusicBrainzException as exc:
         raise HTTPServiceUnavailable from exc
-    
-    # 2. Iterate through the release once to start grouping them by release-group
+
+    # 2. Iterate once through the releases to group them by release-group
     release_groups_by_id = {}
     release_groups = []
     for _release in releases:
-            release_group = ReleaseGroupApiResponse(_release["release-group"])
-            release_groups_by_id[release_group.mbid] = {
-                "mbid": release_group.mbid,
-                "name": release_group.name,
-                "year": release_group.year,
-                "release_count": 0,
-            }
-            release_groups.append(release_group.mbid)
+        release_group = ReleaseGroupApiResponse(_release["release-group"])
+        release_groups_by_id[release_group.mbid] = {
+            "mbid": release_group.mbid,
+            "name": release_group.name,
+            "year": release_group.year,
+            "release_count": 0,
+        }
+        release_groups.append(release_group.mbid)
 
     # No point in continuing if there are no release groups
     if not release_groups:
         return {"albums": []}
-    
-    # Update the release count of each, this time just iterate through the
-    # distinct release groups
+
+    # 3. Update the release count of each release-group, this time just
+    # iterating through the distinct release groups
     for release_group_id, count in Counter(release_groups).items():
         release_groups_by_id[release_group_id]["release_count"] = count
 
     # Sort by release count descending, to then slice the list for pagination
-    albums = sorted(
+    release_groups = sorted(
         release_groups_by_id.values(),
         key=lambda group: group["release_count"],
-        reverse=True
-    )[offset:offset + limit]
+        reverse=True,
+    )[offset : offset + limit]
+    return {"albums": release_groups}
 
-    return {"albums": albums}
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     albums.interface.cli()
